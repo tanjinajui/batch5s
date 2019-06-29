2e0b42218795325a7e4420e75e83ba2fac536d64
tanjinajui <tanjinaakter994@gmail.com>
2019-06-30 01:30:36 +0600

    RefrigeratorApp

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        Refrigerator refrigeratorobj = new Refrigerator();
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigeratorobj.ItemsNo = Convert.ToInt32(itemTextBox.Text);
+            itemTextBox.Clear();
+            weightTextBox.Clear();
+            if ((refrigeratorobj.validation()) == true)
+            {
+                MessageBox.Show("SorryItems Weight is larger than Refrigerators Maximum Weight Limit");
+            }
+            else
+            {
+                currentWeightTextBox.Text = Convert.ToString(refrigeratorobj.GetCurrentWeight());
+                remainingWeightTextBox.Text = Convert.ToString(refrigeratorobj.GetRemainingWeight());
+            }
+        }
+    }
+}
